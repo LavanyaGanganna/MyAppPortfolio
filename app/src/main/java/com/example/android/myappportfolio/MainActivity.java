5f7318842fe953dd6d878434154d52fde496db7e
Lavanya Bg <lavanyabg@gmail.com>
2016-08-24 12:28:59 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.android.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+	Button moviebutton,stockbutton,buildbutton,materialbutton,ubiquitiousbutton,capstonebutton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		moviebutton= (Button) findViewById(R.id.movieapp_button);
+		stockbutton=(Button)findViewById(R.id.stockhawk_button);
+		buildbutton=(Button)findViewById(R.id.buildbigger_button);
+		materialbutton=(Button)findViewById(R.id.materialdesign_button);
+		ubiquitiousbutton=(Button)findViewById(R.id.ubiquitious_button);
+		capstonebutton= (Button) findViewById(R.id.capstone_button);
+		moviebutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this,"This button will launch my popular movies app",Toast.LENGTH_SHORT).show();
+			}
+		});
+		stockbutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this,"This button will launch my stockhawk app",Toast.LENGTH_SHORT).show();
+			}
+		});
+		buildbutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this,"This button will launch my build bigger app",Toast.LENGTH_SHORT).show();
+
+			}
+		});
+		materialbutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this,"This button will launch my material design app",Toast.LENGTH_SHORT).show();
+			}
+		});
+		ubiquitiousbutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this,"This button will launch my Go ubiquitious app",Toast.LENGTH_SHORT).show();
+			}
+		});
+		capstonebutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this,"This button will launch my Capstone app",Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+}
